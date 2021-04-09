85cbce499e615ca3a75fb7f84b77c901f9bacf4f
Tes3awy <osaid2512@gmail.com>
2021-04-09 19:58:23 +0200

    Change code into a function

--- cisco_config_generator.py
+++ cisco_config_generator.py
@@ -19,72 +19,85 @@
 from os import path, mkdir, stat
 from csv import DictReader
 from jinja2 import Environment, FileSystemLoader
-import json
 import webbrowser as TextEditor
 from time import sleep
 
 # Global Vars
-output_dir = "configs"
-jinja_template = "switch.j2"
-params_file = "CSV/01. params.csv"
-vlans_file = "CSV/02. vlans.csv"
-etherchannels_file = "CSV/03. etherchannels.csv"
-port_mapping = "CSV/04. port_mapping.csv"
+OUTPUT_DIR = "configs"
+JINJA_TEMPLATE = "switch.j2"
+PARAMS_FILE = "CSV/01. params.csv"
+VLANS_FILE = "CSV/02. vlans.csv"
+ETHERCHANNELS_FILE = "CSV/03. etherchannels.csv"
+PORT_MAPPING = "CSV/04. port_mapping.csv"
 
-# Handle Jinja template
-env = Environment(loader=FileSystemLoader("./"), trim_blocks=True, lstrip_blocks=True)
-template = env.get_template(jinja_template)
 
-# Create configs directory if not already created
-if not path.exists(output_dir):
-    mkdir(output_dir)
+def build_template(
+    template_file, parameters_file, vlans_file, etherchannels_file, port_mapping
+):
 
-# Read the "parameters" csv sheet and convert it to a dict
-with open(params_file, "r") as parameters:
-    params = DictReader(parameters)
-    for param in params:
-        dict_params = dict(param)
+    # Handle Jinja template
+    env = Environment(
+        loader=FileSystemLoader("./"), trim_blocks=True, lstrip_blocks=True
+    )
+    template = env.get_template(JINJA_TEMPLATE)
 
-# Read the "vlans" csv sheet and convert it to a dict
-with open(vlans_file, "r") as vlans:
-    vlans = DictReader(vlans)
-    dict_vlans = {"vlans": []}
-    for vlan in vlans:
-        dict_vlans["vlans"].append(dict(vlan))
+    # Create configs directory if not already created
+    if not path.exists(OUTPUT_DIR):
+        mkdir(OUTPUT_DIR)
 
-# Read the "etherchannels" csv sheet and convert it to a dict
-with open(etherchannels_file, "r") as etherchannels:
-    portchannels = DictReader(etherchannels)
-    dict_portchannels = {"etherchannels": []}
-    for portchannel in portchannels:
-        dict_portchannels["etherchannels"].append(dict(portchannel))
+    # Read the "parameters" csv sheet and convert it to a dict
+    with open(PARAMS_FILE, "r") as parameters:
+        params = DictReader(parameters)
+        for param in params:
+            dict_params = dict(param)
 
-# Read the "port mapping" csv sheet and convert it to a dict
-with open(port_mapping, "r") as interfaces:
-    ports = DictReader(interfaces)
-    dict_ports = {"interfaces": []}
-    for port in ports:
-        dict_ports["interfaces"].append(dict(port))
+    # Read the "vlans" csv sheet and convert it to a dict
+    with open(VLANS_FILE, "r") as vlans:
+        vlans = DictReader(vlans)
+        dict_vlans = {"vlans": []}
+        for vlan in vlans:
+            dict_vlans["vlans"].append(dict(vlan))
 
-# Combine all four csv sheets
-dict_params.update(dict_vlans)
-dict_params.update(dict_portchannels)
-dict_params.update(dict_ports)
+    # Read the "etherchannels" csv sheet and convert it to a dict
+    with open(ETHERCHANNELS_FILE, "r") as etherchannels:
+        portchannels = DictReader(etherchannels)
+        dict_portchannels = {"etherchannels": []}
+        for portchannel in portchannels:
+            dict_portchannels["etherchannels"].append(dict(portchannel))
 
-# All dictionaries
-dicts = dict_params
+    # Read the "port mapping" csv sheet and convert it to a dict
+    with open(PORT_MAPPING, "r") as interfaces:
+        ports = DictReader(interfaces)
+        dict_ports = {"interfaces": []}
+        for port in ports:
+            dict_ports["interfaces"].append(dict(port))
 
-# Generate the configuration template file
-res = template.render(dicts)
-file_name = dicts["hostname"]
-file_ext = ".ios"
-file_location = path.join(output_dir, file_name + file_ext)
-file = open(file_location, "w")
-file.write(res)
-file.close()
+    # Combine all four csv sheets
+    dict_params.update(dict_vlans)
+    dict_params.update(dict_portchannels)
+    dict_params.update(dict_ports)
 
-print("✔ Configuration file '%s' is created successfully!" % (file_name + file_ext))
+    # All dictionaries
+    dicts = dict_params
 
-# Open file in default Text Editor for the file extension
-sleep(1)
-TextEditor.open(file_location)
+    # Generate the configuration template file
+    res = template.render(dicts)
+    file_name = dicts["hostname"]
+    file_ext = ".ios"
+    file_location = path.join(OUTPUT_DIR, file_name + file_ext)
+    file = open(file_location, "w")
+    file.write(res)
+    file.close()
+
+    print("✔ Configuration file '%s' is created successfully!" % (file_name + file_ext))
+
+    # Open file in default Text Editor for the file extension
+    sleep(1)
+    TextEditor.open(file_location)
+    return True
+
+
+if __name__ == "__main__":
+    build_template(
+        JINJA_TEMPLATE, PARAMS_FILE, VLANS_FILE, ETHERCHANNELS_FILE, PORT_MAPPING
+    )
